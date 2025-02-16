68556784d20aeb607459fc7b67af08c17f3d73a9
war <crypto22h@gmail.com>
2025-02-16 18:11:05 +0000

    implement book entity with basic validation

--- LibrarySystem.Domain/Entities/Book.cs
+++ LibrarySystem.Domain/Entities/Book.cs
@@ -11,7 +11,41 @@
 
         public Book(string title, string author, DateTime publicationDate, string isbn, int? editionNumber = null)
         {
-            throw new NotImplementedException();
+            ValidateTitle(title);
+            ValidateAuthor(author);
+            ValidatePublicationDate(publicationDate);
+            ValidateISBN(isbn);
+
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            ISBN = isbn;
+            EditionNumber = editionNumber;
+        }
+
+        private static void ValidateTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title cannot be empty", nameof(title));
+        }
+
+        private static void ValidateAuthor(string author)
+        {
+            if (string.IsNullOrWhiteSpace(author))
+                throw new ArgumentException("Author cannot be empty", nameof(author));
+        }
+
+        private static void ValidatePublicationDate(DateTime publicationDate)
+        {
+            // TODO: double check there aren't any issues to do with using DateTime.UtcNow
+            if (publicationDate > DateTime.UtcNow)
+                throw new ArgumentException("Publication date cannot be in the future", nameof(publicationDate));
+        }
+
+        private static void ValidateISBN(string isbn)
+        {
+            if (string.IsNullOrWhiteSpace(isbn))
+                throw new ArgumentException("ISBN cannot be empty", nameof(isbn));
         }
     }
 }
